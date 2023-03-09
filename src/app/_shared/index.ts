35a6927f1eed134a759c8b18c7da61d75fbb0258
Mawreck <marek.ounpuu@srini.ee>
2023-03-09 03:50:48 +0200

    feat(shared): add index

---
+++ src/app/_shared/index.ts
@@ -0,0 +1,11 @@
+export { AlbumsService } from "./services/albums/albums.service";
+export { Album } from "./services/albums/albums.model";
+export { Photo } from "./services/albums/albums.model";
+
+export { LogsService } from "./services/logs/logs.service";
+export { Log } from "./services/logs/logs.model";
+
+export { UsersService } from "./services/users/users.service";
+export { User } from "./services/users/users.model";
+
+export { Paged } from "./utils/utils.models";
