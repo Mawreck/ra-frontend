fd56f0d4a599a282dbd880212f94de058cc747b5
Mawreck <marek.ounpuu@srini.ee>
2023-03-09 03:57:17 +0200

    feat(services): add logs service

---
+++ src/app/_shared/services/logs/logs.model.ts
@@ -0,0 +1,22 @@
+import { Paged } from "../../utils/utils.models";
+
+export type LogType = "ALBUMS"
+  | "PHOTOS";
+
+export type ActionType = "ADD"
+  | "DELETE";
+
+export interface Log {
+  username: string;
+  logType: LogType;
+  logTypeId: number;
+  action: ActionType;
+  dateCreated?: Date;
+}
+
+export interface LogResponse {
+  page: Paged,
+  _embedded: {
+    logs: Log[]
+  }
+}
---
+++ src/app/_shared/services/logs/logs.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { map, Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Log, LogResponse } from "./logs.model";
+
+@Injectable({
+  providedIn: "root"
+})
+export class LogsService {
+
+  private readonly BASE_URL = "http://localhost:8080/api";
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+  }
+
+  postLog(log: Log): Observable<Log> {
+    return this.httpClient.post<Log>(`${ this.BASE_URL }/logs`, log);
+  }
+
+  getLogs(): Observable<Log[]> {
+    return this.httpClient.get<LogResponse>(`${ this.BASE_URL }/logs`)
+      .pipe(
+        map(data => data._embedded.logs)
+      );
+  }
+
+}
